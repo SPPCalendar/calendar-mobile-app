ae8a51a924eed3930671d5c28dc367a65a084003
sofiiabudilova <sofiia.budilova@akra.de>
2025-04-09 23:34:03 +0300

    🚧 Add month numbers function

---
+++ utils/utils.ts
@@ -0,0 +1,86 @@
+// import { Month } from "@/enums/Month";
+
+export enum Month {
+  January = 1,
+  February,
+  March,
+  April,
+  May,
+  June,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December,
+}
+
+type MonthNumbers = {
+  lastMonth: number[];
+  thisMonth: number[][];
+  nextMonth: number[];
+};
+
+const getMonthNumbers = (month: Month, year: number) => {
+  const result: MonthNumbers = { lastMonth: [], thisMonth: [], nextMonth: [] };
+  const firstDay = new Date(year, month - 1, 1);
+  const lastDay = new Date(year, month, 0);
+
+  const currentDateLastMonth = new Date(firstDay);
+
+  if (firstDay.getDay() != 1) {
+    currentDateLastMonth.setDate(currentDateLastMonth.getDate() - 1);
+    result.lastMonth.push(currentDateLastMonth.getDate());
+
+    while (currentDateLastMonth.getDay() != 1) {
+      currentDateLastMonth.setDate(currentDateLastMonth.getDate() - 1);
+      result.lastMonth.push(currentDateLastMonth.getDate());
+    }
+  }
+  result.lastMonth.sort();
+
+  const currentDateThisMonth = new Date(firstDay);
+
+  if (firstDay.getDay() != 7) {
+    const firstWeek = [];
+    firstWeek.push(firstDay.getDate());
+
+    while (currentDateThisMonth.getDay() != 6) {
+      currentDateThisMonth.setDate(currentDateThisMonth.getDate() + 1);
+      firstWeek.push(currentDateThisMonth.getDate());
+    }
+    currentDateThisMonth.setDate(currentDateThisMonth.getDate() + 1);
+    firstWeek.push(currentDateThisMonth.getDate());
+    result.thisMonth.push(firstWeek);
+  }
+
+  for (let i = 0; i < 4; i++) {
+    const currentWeek = [];
+    while (currentDateThisMonth.getDay() != 6) {
+      currentDateThisMonth.setDate(currentDateThisMonth.getDate() + 1);
+      currentWeek.push(currentDateThisMonth.getDate());
+    }
+    currentDateThisMonth.setDate(currentDateThisMonth.getDate() + 1);
+    currentWeek.push(currentDateThisMonth.getDate());
+    result.thisMonth.push(currentWeek);
+  }
+
+  const lastWeek = [];
+  while (lastDay.getTime() != currentDateThisMonth.getTime()) {
+    currentDateThisMonth.setDate(currentDateThisMonth.getDate() + 1);
+    lastWeek.push(currentDateThisMonth.getDate());
+  }
+  result.thisMonth.push(lastWeek);
+
+  const left = 7 - lastWeek.length;
+
+  for (let i = 1; i < left + 1; i++) {
+    result.nextMonth.push(i);
+  }
+
+  console.log(result.lastMonth);
+  console.log(result.thisMonth);
+  console.log(result.nextMonth);
+};
+
+getMonthNumbers(Month.March, 2025);
