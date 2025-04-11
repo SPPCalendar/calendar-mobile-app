56b793a54e1894a0d32fa6aad01a023fe3e6c026
Volodymyr Beimuk <tetrenkuchioan@gmail.com>
2025-04-11 18:21:29 +0300

    handle redirect to login when creating new events

--- app/presentation/_layout.tsx
+++ app/presentation/_layout.tsx
@@ -3,13 +3,25 @@ import TopBar from "@/components/TopBars/TopBar";
 import { Colors } from "@/contants/Colors";
 import { Href, router, Stack } from "expo-router";
 import { SafeAreaView, View } from "react-native";
+import { useAuthStore } from "@/stores/auth_store";
 
 export default function RootLayout() {
+  const accessToken = useAuthStore((state) => state.accessToken);
+
+  const handleAddEventButtonPress = () => {
+    if (!accessToken) {
+      router.push("/login" as Href);
+      return;
+    }
+
+    router.push("/event_forms/new_event_form" as Href);
+  };
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: Colors.backgroundColor }}>
       <TopBar />
       <Stack screenOptions={{ headerShown: false }} />
-      <AddEventButton onPress={() => router.push('/event_forms/new_event_form' as Href)} />
+      <AddEventButton onPress={handleAddEventButtonPress} />
       
     </SafeAreaView>
   );
