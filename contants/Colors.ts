9a0c8e6d64bf164c456c67a9c384c86814530d35
Sofiia Budilova <avolidub@gmail.com>
2025-04-14 14:35:21 +0300

    ✨ Change calendar colors

--- contants/Colors.ts
+++ contants/Colors.ts
@@ -11,3 +11,26 @@ export const Colors = {
   textInputPlaceholder: "#5E5E5E",
   filledCheckboxBg: BaseColors.darkGreen,
 };
+
+export const calendarColors: string[] = [
+  "#FFDD00", // Bright Yellow
+  "#0066FF", // Electric Blue
+  "#FF0033", // Bright Red
+  "#00FF00", // Lime Green
+  "#FF3399", // Hot Pink
+  "#39FF14", // Neon Green
+  "#FF6EC7", // Neon Pink
+  "#1F75FE", // Electric Blue
+  "#9B00FF", // Bright Purple
+  "#FF5F00", // Neon Orange
+  "#FF5733", // Bright Red
+  "#FFFC00", // Bright Yellow
+  "#00BFFF", // Sky Blue
+  "#FFA500", // Bright Orange
+  "#32CD32", // Lime Green
+  "#FF1493", // Vivid Pink
+  "#FF6347", // Orange
+  "#FFD700", // Golden Yellow
+  "#FF7F50", // Coral
+  "#800080", // Bright Purple
+];
