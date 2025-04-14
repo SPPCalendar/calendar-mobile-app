e4440b9628ed3b5ccdb53f2b925c2e96540e050b
sofiiabudilova <sofiia.budilova@akra.de>
2025-04-14 05:07:55 +0300

    ✨ Add events search

--- components/DayEventsList.tsx
+++ components/DayEventsList.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ScrollView, StyleProp, Text, ViewStyle } from "react-native";
 import EventRow from "./EventRow";
 import { CalendarEvent } from "@/types/CalendarEvent";
+import { Colors } from "@/contants/Colors";
 
 interface MonthNameDisplayProps {
   events: CalendarEvent[];
@@ -10,7 +11,12 @@ interface MonthNameDisplayProps {
 
 const DayEventsList: React.FC<MonthNameDisplayProps> = ({ events, style }) => {
   return (
-    <ScrollView style={[{ width: "100%" }, style]}>
+    <ScrollView
+      style={[
+        { width: "100%", backgroundColor: Colors.backgroundColor },
+        style,
+      ]}
+    >
       {events.map((event) => (
         <EventRow key={event.id} event={event} />
       ))}
